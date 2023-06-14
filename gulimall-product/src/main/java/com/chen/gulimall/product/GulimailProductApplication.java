50350b1c2bf17a2293078a68a3f6e6d349086022
ccj-007 <596487930@qq.com>
2023-06-14 22:47:34 +0800

    feat:分类的展示

--- gulimall-product/src/main/java/com/chen/gulimall/product/GulimailProductApplication.java
+++ gulimall-product/src/main/java/com/chen/gulimall/product/GulimailProductApplication.java
@@ -3,12 +3,14 @@ package com.chen.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 1. 整和mybatis-plus 依赖
  * 2. 配置数据源 application.yml
  * 3. 使用mapperscan找到对应路径的dao
  */
+@EnableDiscoveryClient
 @MapperScan("com.chen.gulimall.product.dao")
 @SpringBootApplication
 public class GulimailProductApplication {
