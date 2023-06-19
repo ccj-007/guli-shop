9e8164148c1244b9c328e53bfb571a6d0592935f
ccj-007 <596487930@qq.com>
2023-06-19 22:52:43 +0800

    feat:gateway

---
+++ gulimall-gateway/src/test/java/com/chen/gulimall/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.chen.gulimall.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GulimallGatewayApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+}
---
+++ gulimall-third-party/src/test/java/com/chen/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.gulimall.thirdparty;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GulimallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
